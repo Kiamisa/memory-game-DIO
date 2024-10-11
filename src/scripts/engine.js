c71c12189e7067edf7a22d7b79ef0cd588166881
Kiamisa <silas.particular05@gmail.com>
2024-10-11 11:53:32 -0300

    initial commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,56 @@
+const emojis = [
+    "💛",
+    "💛",
+    "💚",
+    "💚",
+    "💙",
+    "💙",
+    "💜",
+    "💜",
+    "🤎",
+    "🤎",
+    "🖤",
+    "🖤",
+    "🤍",
+    "🤍",
+    "💖",
+    "💖",
+];
+let openedCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1));
+
+for (let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if (openedCards.length < 2){
+        this.classList.add("boxOpen");
+        openedCards.push(this);
+    }
+
+    if(openedCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    if (openedCards[0].innerHTML === openedCards[1].innerHTML){
+        openedCards[0].classList.add("boxMatch");
+        openedCards[1].classList.add("boxMatch");
+    } else {
+        openedCards[0].classList.remove("boxOpen");
+        openedCards[1].classList.remove("boxOpen");
+    }
+
+    openedCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu!");
+    }
+}
